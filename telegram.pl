b5836e1e09f8576a0f508b911c161d01772ce6ba
sansaoipb <sansaoipb@gmail.com>
2017-04-26 14:16:02 -0300

    Add files via upload

--- telegram.pl
+++ telegram.pl
@@ -155,7 +155,8 @@ sub tipo {
 	   	method  => 'item.get',
 		params  => {
 			output => ['value_type'],
-			itemids => $itemid
+			itemids => $itemid,
+			webitems=> $itemid
      		},
    		auth => $authID,
 		id => 2
